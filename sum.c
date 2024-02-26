fd179f61351e6ac17aea093206c044049057ff11
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-02-26 13:26:43 +0530

    sum.c
    WAP input 2 number and display the sum

---
+++ sum.c
@@ -0,0 +1,13 @@
+/*WAP input 2 number and display the sum */
+
+
+
+
+#include<stdio.h>
+void main(){
+    int a=2,b=3,c;
+    
+    c=a+b;
+    printf("addition of %d and %d  is :%d ",a,b,c);
+    
+}
