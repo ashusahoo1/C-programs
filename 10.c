a60e2b17d69c37a8d2964002c0d7a4b81f00a281
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-02-27 01:28:14 +0530

    WAP input dist in m and convert in into km

---
+++ 10.c
@@ -0,0 +1,14 @@
+/*WAP input dist in m and convert in into km*/
+
+
+
+
+#include<stdio.h>
+void main(){
+    float km,m;
+    printf("enter meter:\n");
+    scanf("%f",&m);
+
+    km=m/1000;
+     printf("m converted into km is: %.2f",km);
+}
