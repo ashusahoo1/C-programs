afd64d95a3e950303da53865c15588a0c76ed975
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-02-27 01:16:23 +0530

    WAP input 2 number,swap them without third variable

---
+++ 7.c
@@ -0,0 +1,14 @@
+/*WAP input 2 number,swap them without third variable */
+
+
+
+
+#include<stdio.h>
+void main(){
+    int a=10,b=20;
+    printf("before swap a=%d,b=%d",a,b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("\n after swap a=%d,b=%d",a,b);
+}
