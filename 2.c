539a29822e10cb33c131ef8b9c2ed7de18fff755
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-02-26 13:14:22 +0530

    Create 2.c
    print hello world using new line command

---
+++ 2.c
@@ -0,0 +1,10 @@
+/*write a program to print hello world in first line and 
+welcome to kiit in second line using single printf */
+
+
+
+
+#include<stdio.h>
+void main(){
+    printf("hello world!\n welcome to kiit");
+}
