0cc5869d85e167484bd9456ca22be602d834212a
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-02-26 17:03:56 +0530

    4.c
    WAP input 2 number and find out the sum

---
+++ 4.c
@@ -0,0 +1,16 @@
+/*WAP input 2 number and find out the sum */
+
+
+
+
+#include<stdio.h>
+void main(){
+    int a,b,c;
+    
+    
+    printf("enter two number:\n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("addition of %d and %d is:%d",a,b,c);
+    
+}
