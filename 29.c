d103e148871094e2df5aae97c3e5a81173dbef0c
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-03-13 21:40:49 +0530

    LOOP
    display the number 123..9 on screen in diff line using while

---
+++ 29.c
@@ -0,0 +1,11 @@
+/*display the number 123..9 on screen in diff line using while*/
+
+#include<stdio.h>
+void main(){
+    int i=1;
+    while(i<10){
+        printf("%d\n",i);
+        i=i+1;
+    }
+
+}
