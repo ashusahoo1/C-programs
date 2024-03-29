52c3e7a019220a2e6d8aeab12d22a333abae1146
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-03-29 13:45:01 +0530

    WAP to display = 10 times on screen

---
+++ 48.c
@@ -0,0 +1,16 @@
+/*WAP to display = 10 times on screen*/
+
+
+
+#include <stdio.h>
+
+void disp(int n){
+    int i;
+    for(i=0;i<=n;i++)
+    printf("=\n");
+   
+
+}
+void main(){
+    disp(10);
+}
