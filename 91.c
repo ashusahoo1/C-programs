643d5b90996b228289e46da8fd56c8fffcf8d49a
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-06-18 01:09:15 +0530

    WAP input two string state whether both string are  identical or not also show not identical

---
+++ 91.c
@@ -0,0 +1,23 @@
+/*WAP input two string state whether both string are 
+identical or not also show not identical*/
+
+
+#include <stdio.h>  
+#include<string.h>  
+int main()  
+{  
+   char str1[20];  // declaration of char array  
+   char str2[20];  // declaration of char array  
+   int value; // declaration of integer variable  
+   printf("Enter the first string : ");  
+   scanf("%s",str1);  
+   printf("Enter the second string : ");  
+   scanf("%s",str2);  
+   // comparing both the strings using strcmp() function  
+   value=strcmp(str1,str2);  
+   if(value==0)  
+   printf("strings are same");  
+   else  
+   printf("strings are not same");  
+   return 0;  
+}
