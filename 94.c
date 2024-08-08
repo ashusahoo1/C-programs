6931b6b0176dbfb80692b8b2e3a6781e0220a377
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-08-09 00:02:28 +0530

    Write a program in C to store elements in an array and print them. Test Data :

---
+++ 94.c
@@ -0,0 +1,23 @@
+/*
+
+1. Write a program in C to store elements in an array and print them.
+Test Data :
+Input 10 elements in the array :
+element - 0 : 1
+element - 1 : 1
+element - 2 : 2
+.......
+Expected Output :
+Elements in array are: 1 1 2 3 4 5 6 7 8 9
+
+*/
+
+
+
+#include<stdio.h>
+int main(){
+    int a[10]={1,2,3,4,5,6,7,8,9,10};
+    for(int i=0;i<10;i++){
+        printf("%d ",a[i]);
+    }
+}
