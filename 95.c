238ab95f867d6081c4358d671f03b0af69b3c782
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-08-10 00:18:21 +0530

    . Write a program in C to read n number of values in an array and display them in reverse order.

---
+++ 95.c
@@ -0,0 +1,23 @@
+/*
+2. Write a program in C to read n number of values in an array and display them in reverse order.
+Test Data :
+Input the number of elements to store in the array :3
+Input 3 number of elements in the array :
+element - 0 : 2
+element - 1 : 5
+element - 2 : 7
+Expected Output :
+The values store into the array are :
+2 5 7
+The values store into the array in reverse are :
+7 5 2
+*/
+
+
+#include<stdio.h>
+int main(){
+    int a[10]={1,2,3,5,6,7};
+    for(int i=5;i>=0;i--){
+        printf("%d ",a[i]);
+    }
+}
